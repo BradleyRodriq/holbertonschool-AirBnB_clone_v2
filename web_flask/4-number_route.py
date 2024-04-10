2c6de9302257258bb207c0243058e64fdac03168
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-04-10 18:33:49 -0400

    added only int to task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -30,7 +30,7 @@ def pyfun(text):
     return 'Python ' + text.replace('_', ' ')
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def its_a_num(n):
     """ returns the number """
     return str(n).replace('_', ' ') + " " + "is a number"
