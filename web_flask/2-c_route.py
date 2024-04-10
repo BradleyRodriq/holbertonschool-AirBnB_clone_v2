ba3d339953f6091618bb597b204a8fe594e1888e
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-04-10 18:27:39 -0400

    same task 0 fix on other files

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
-from flask import Flask
 """ starts the flask app """
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -9,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_world():
     """ returns hello HBNB """
-    return 'Hello HBNB'
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
+""" starts the flask app """
 from flask import Flask
 from flask import render_template
-""" starts the flask app """
 
 app = Flask(__name__)
 
@@ -10,7 +9,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_world():
     """ returns hello HBNB """
-    return 'Hello HBNB'
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
