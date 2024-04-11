2ba605056c0e9d8876ced849c42e677657393d06
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-04-10 23:35:20 -0400

    task 11 saving progress

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" starts a web flask app """
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """ displays the main filters HTML page """
+    states = storage.all("State")
+    amenities = storage.all("Amenities")
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
+
+
+app.teardown_appcontext
+def teardown(exc):
+    """ removes current SQLA session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
